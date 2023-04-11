5cedbc1b307adc53b32a529be06c8d3fb7ef7f4d
hoangndst <hoangndst@outlook.com>
2023-04-11 21:45:35 +0700

    mnk

---
+++ play.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+import sys
+import time
+import argparse
+from typing import Dict, Tuple
+import torch
+
+import config as cfg
+from lib import model, utils
+from lib.game import game_provider
+
+
+WinLoseDraw = Tuple[int, int, int]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "models", nargs='+', help="The list of models (at least 2) to play against each other")
+    parser.add_argument("-r", "--rounds", type=int, default=2,
+                        help="Count of rounds to perform for every pair")
+    parser.add_argument("--cuda", default=False,
+                        action="store_true", help="Enable CUDA")
+    game_provider.add_game_argument(parser)
+    args = parser.parse_args()
+    device = "cuda" if args.cuda else "cpu"
+
+    game = game_provider.get_game(args)
+
+    nets = []
+    for fname in args.models:
+        net = model.Net(game.obs_shape, game.action_space)
+        net.load_state_dict(torch.load(
+            fname, map_location=lambda storage, loc: storage))
+        net = net.to(device)
+        nets.append((fname, net))
+
+    total_agent: Dict[str, WinLoseDraw] = {}
+    total_pairs: Dict[Tuple[str, str], WinLoseDraw] = {}
+
+    for idx1, n1 in enumerate(nets):
+        for idx2, n2 in enumerate(nets):
+            if idx1 == idx2:
+                continue
+            wins, losses, draws = 0, 0, 0
+            ts = time.time()
+            for _ in range(args.rounds):
+                r, _ = utils.play_game(game=game, mcts_stores=None, replay_buffer=None,
+                                       net1=n1[1], net2=n2[1],
+                                       steps_before_tau_0=0,
+                                       mcts_searches=cfg.PLAY_MCTS_SEARCHES,
+                                       mcts_batch_size=cfg.PLAY_MCTS_BATCH_SIZE,
+                                       device=device)
+                if r > 0.5:
+                    wins += 1
+                elif r < -0.5:
+                    losses += 1
+                else:
+                    draws += 1
+            speed_games = args.rounds / (time.time() - ts)
+            name_1, name_2 = n1[0], n2[0]
+            print("%s vs %s -> w=%d, l=%d, d=%d" %
+                  (name_1, name_2, wins, losses, draws))
+            sys.stderr.write("Speed %.2f games/s\n" % speed_games)
+            sys.stdout.flush()
+            utils.update_counts(total_agent, name_1, (wins, losses, draws))
+            utils.update_counts(total_agent, name_2, (losses, wins, draws))
+            utils.update_counts(
+                total_pairs, (name_1, name_2), (wins, losses, draws))
+
+    # leaderboard by total wins
+    total_leaders = list(total_agent.items())
+    total_leaders.sort(reverse=True, key=lambda p: p[1][0])
+
+    print("Leaderboard:")
+    for name, (wins, losses, draws) in total_leaders:
+        print("%s: \t w=%d, l=%d, d=%d" % (name, wins, losses, draws))
---
+++ telegram-bot.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python3
+import os
+import sys
+import glob
+import json
+import time
+import datetime
+import random
+import logging
+import numpy as np
+import configparser
+import argparse
+
+import config as cfg
+from lib import utils, play_session
+from lib.game import game_provider
+
+
+try:
+    import telegram.ext
+    from telegram.error import TimedOut
+except ImportError:
+    print("You need python-telegram-bot package installed to start the bot")
+    sys.exit()
+
+
+# Configuration file with the following contents
+# [telegram]
+# api=API_KEY
+CONFIG_DEFAULT = "./.config/bot.ini"
+
+log = logging.getLogger("telegram")
+
+
+class PlayerBot:
+    def __init__(self, game, models_dir, log_file):
+        self.game = game
+        self.player_id = None
+        self.sessions = {}
+        self.models_dir = models_dir
+        self.models = self._read_models(models_dir)
+        self.log_file = log_file
+        self.leaderboard = {}
+        self._read_leaderboard(log_file)
+
+    def _read_models(self, models_dir):
+        result = {}
+        for idx, name in enumerate(sorted(glob.glob(os.path.join(models_dir, "*.dat")))):
+            result[idx] = name
+        return result
+
+    def _read_leaderboard(self, log_file):
+        if not os.path.exists(log_file):
+            return
+        with open(log_file, 'rt', encoding='utf-8') as fd:
+            for l in fd:
+                data = json.loads(l)
+                bot_name = os.path.basename(data['model_file'])
+                user_name = data['player_id'].split(':')[0]
+                bot_score = data['bot_score']
+                self._update_leaderboard(bot_score, bot_name, user_name)
+
+    def _update_leaderboard(self, bot_score, bot_name, user_name):
+        if bot_score > 0.5:
+            utils.update_counts(self.leaderboard, bot_name, (1, 0, 0))
+            utils.update_counts(self.leaderboard, user_name, (0, 1, 0))
+        elif bot_score < -0.5:
+            utils.update_counts(self.leaderboard, bot_name, (0, 1, 0))
+            utils.update_counts(self.leaderboard, user_name, (1, 0, 0))
+        else:
+            utils.update_counts(self.leaderboard, bot_name, (0, 0, 1))
+            utils.update_counts(self.leaderboard, user_name, (0, 0, 1))
+
+    def _save_log(self, session, bot_score):
+        self._update_leaderboard(bot_score, os.path.basename(session.model_file),
+                                 self.player_id.split(':')[0])
+        data = {
+            "ts": time.time(),
+            "time": datetime.datetime.utcnow().isoformat(),
+            "bot_score": bot_score,
+            "model_file": session.model_file,
+            "player_id": self.player_id,
+            "player_moves_first": session.player_moves_first,
+            "moves": session.moves,
+            "state": session.state
+        }
+        with open(self.log_file, "a+t", encoding='utf-8') as f:
+            f.write(json.dumps(data, sort_keys=True) + '\n')
+
+    def command_help(self, bot, update):
+        bot.send_message(chat_id=update.message.chat_id, parse_mode="HTML", disable_web_page_preview=True,
+                         text="""
+This a <a href="https://en.wikipedia.org/wiki/Connect_Four">4-in-a-row</a> game bot trained with AlphaGo Zero method for the <a href="https://www.packtpub.com/big-data-and-business-intelligence/practical-deep-reinforcement-learning">Practical Deep Reinforcement Learning</a> book.
+
+<b>Welcome!</b>
+
+This bot understands the following commands:
+<b>/list</b> to list available pre-trained models (the higher the ID, the stronger the play)
+<b>/play MODEL_ID</b> to start the new game against the specified model
+<b>/top</b> show leaderboard
+
+During the game, your moves are numbers of columns to drop the disk.
+""")
+
+    def command_list(self, bot, update):
+        if len(self.models) == 0:
+            reply = ["There are no models currently available, sorry!"]
+        else:
+            reply = ["The list of available models with their IDs"]
+            for idx, name in sorted(self.models.items()):
+                reply.append("<b>%d</b>: %s" % (idx, os.path.basename(name)))
+
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="\n".join(reply), parse_mode="HTML")
+
+    def command_play(self, bot, update, args):
+        chat_id = update.message.chat_id
+        self.player_id = "%s:%s" % (
+            update.message.from_user.username, update.message.from_user.id)
+        try:
+            model_id = int(args[0])
+        except (ValueError, IndexError):
+            bot.send_message(
+                chat_id=chat_id, text="Wrong argumants! Use '/play <MODEL_ID>, to start the game")
+            return
+
+        if model_id not in self.models:
+            bot.send_message(
+                chat_id=chat_id, text="There is no such model, use /list command to get list of IDs")
+            return
+
+        if chat_id in self.sessions:
+            bot.send_message(
+                chat_id=chat_id, text="You already have the game in progress, it will be discarded")
+            del self.sessions[chat_id]
+
+        player_moves = random.choice([False, True])
+        session = play_session.Session(
+            self.game, self.models[model_id], player_moves)
+        self.sessions[chat_id] = session
+        if player_moves:
+            bot.send_message(
+                chat_id=chat_id, text=f"Your move is first (you're playing with O), please give the position of your move - single number from 0 to {self.game.action_space}")
+        else:
+            bot.send_message(
+                chat_id=chat_id, text="The first move is mine (I'm playing with X), moving...")
+            session.move_bot()
+        bot.send_message(
+            chat_id=chat_id, text=session.render(), parse_mode="HTML")
+
+    def text(self, bot, update):
+        chat_id = update.message.chat_id
+
+        if chat_id not in self.sessions:
+            bot.send_message(chat_id=chat_id, text="You have no game in progress. Start it with <b>/play MODEL_ID</b> "
+                                                   "(or use <b>/help</b> to see the list of commands)",
+                             parse_mode='HTML')
+            return
+        session = self.sessions[chat_id]
+
+        try:
+            move = int(update.message.text)
+        except ValueError:
+            bot.send_message(chat_id=chat_id, text="I don't understand. In play mode you can give a number "
+                                                   "to specify your move.")
+            return
+
+        if not session.is_valid_move(move):
+            bot.send_message(
+                chat_id=chat_id, text="Move %d is invalid!" % move)
+            return
+
+        won = session.move_player(move)
+        if won:
+            bot.send_message(chat_id=chat_id, text="You won! Congratulations!")
+            self._save_log(session, bot_score=-1)
+            del self.sessions[chat_id]
+            return
+
+        won = session.move_bot()
+        bot.send_message(
+            chat_id=chat_id, text=session.render(), parse_mode="HTML")
+
+        if won:
+            bot.send_message(chat_id=chat_id, text="I won! Wheeee!")
+            self._save_log(session, bot_score=1)
+            del self.sessions[chat_id]
+        # checking for a draw
+        if session.is_draw():
+            bot.send_message(
+                chat_id=chat_id, text="Draw position. 1:1, see ya!")
+            self._save_log(session, bot_score=0)
+            del self.sessions[chat_id]
+
+    def error(self, bot, update, error):
+        try:
+            raise error
+        except TimedOut:
+            log.info("Timed out error")
+
+    def command_top(self, bot, update):
+        res = ["Leader board"]
+        items = list(self.leaderboard.items())
+        items.sort(reverse=True, key=lambda p: p[1][0])
+        for user, (wins, losses, draws) in items:
+            res.append("%20s: won=%d, lost=%d, draw=%d" %
+                       (user[:20], wins, losses, draws))
+        l = "\n".join(res)
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="<pre>" + l + "</pre>", parse_mode="HTML")
+
+    def command_refresh(self, bot, update):
+        self.models = self._read_models(self.models_dir)
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Models reloaded, %d files have found" % len(self.models))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)-15s %(levelname)s %(message)s", level=logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", default=CONFIG_DEFAULT,
+                        help="Configuration file for the bot, default=" + CONFIG_DEFAULT)
+    parser.add_argument("-m", "--models", required=True,
+                        help="Directory name with models to serve")
+    parser.add_argument("-l", "--log", default=f'logs/{time.strftime("%Y%m%d-%H%M%S")}.log',
+                        help="Log name to keep the games and leaderboard")
+    game_provider.add_game_argument(parser)
+    args = parser.parse_args()
+
+    conf = configparser.ConfigParser()
+    if not conf.read(os.path.expanduser(args.config)):
+        log.error("Configuration file %s not found", args.config)
+        sys.exit()
+
+    game = game_provider.get_game(args)
+    player_bot = PlayerBot(game, args.models, args.log)
+
+    updater = telegram.ext.Updater(conf['telegram']['api'])
+    updater.dispatcher.add_handler(
+        telegram.ext.CommandHandler('help', player_bot.command_help))
+    updater.dispatcher.add_handler(
+        telegram.ext.CommandHandler('list', player_bot.command_list))
+    updater.dispatcher.add_handler(
+        telegram.ext.CommandHandler('top', player_bot.command_top))
+    updater.dispatcher.add_handler(telegram.ext.CommandHandler(
+        'play', player_bot.command_play, pass_args=True))
+    updater.dispatcher.add_handler(telegram.ext.CommandHandler(
+        'refresh', player_bot.command_refresh))
+    updater.dispatcher.add_handler(telegram.ext.MessageHandler(
+        telegram.ext.Filters.text, player_bot.text))
+    updater.dispatcher.add_error_handler(player_bot.error)
+
+    log.info("Bot initialized, started serving")
+    updater.start_polling()
+    updater.idle()
